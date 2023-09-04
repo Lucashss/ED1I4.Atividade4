42aa50a8b5363ec10c4b108b3bb39a4b70e68e5e
Lucas <Lucas@LUCAS-PC>
2023-09-03 22:25:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ED1I4.Atividade4
+{
+    static class Program
+    {
+    /*
+     *PARTICIPANTES
+     * LUCAS HENRIQUE DE SOUZA SANTOS CB3012212
+     * KETHELEEN CRISTINE SIMÃO DOS SANTOS
+    */
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
